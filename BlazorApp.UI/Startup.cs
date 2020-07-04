2ce82d1f06322a041f6d6dac77d9b5d521851644
maradonitza <maradonitza@gmail.com>
2020-07-05 00:33:54 +0300

    clean project

--- BlazorApp.UI/Startup.cs
+++ BlazorApp.UI/Startup.cs
@@ -9,7 +9,6 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using BlazorApp.UI.Data;
 
 namespace BlazorApp.UI
 {
@@ -28,7 +27,6 @@ namespace BlazorApp.UI
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
