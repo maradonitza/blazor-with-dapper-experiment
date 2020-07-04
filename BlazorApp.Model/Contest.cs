83effee82fa485f6dced4bafcaace20cfc7cb0c9
maradonitza <maradonitza@gmail.com>
2020-07-05 00:45:01 +0300

    Add ConnectionStrings. Add .NET Standard class libraries BlazorApp.Model and BlazorApp.Data.Dapper. Create new model (Contest class)

---
+++ BlazorApp.Model/Contest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BlazorApp.Model
+{
+    public class Contest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string AdditionalInformation { get; set; }
+    }
+
+}
